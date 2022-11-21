0fc7c90375b2f19d6f36d3fcd60824b8b6ef3f71
Igor Guimarães <igorgui.tec@gmail.com>
2022-11-21 17:11:21 -0300

    nodemon totalmente aplicado

--- src/app.js
+++ src/app.js
@@ -1,10 +1,54 @@
 import express, { json } from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import joi from "joi";
+import bcrypt from 'bcrypt';
+dotenv.config()
 
 const app = express()
-app.use(json())
+app.use(express.json())
 app.use(cors())
 
+const users = [];
 
-app.listen(5000, console.log('Você está utilizando a porta 5000'))
+app.post('/sign-up', async (req, res) => {
+    const { name, password, mail } = req.body
+    const user = {
+        name,
+        password,
+        mail,
+        movs: []
+    }
+    users.push(user)
+    res.send(users)
+})
+
+app.post('/sign-in', async (req, res) => {
+    const {mail, password} = req.body;
+
+   const user = users.find((e) => e.mail === mail)
+    res.send(user)
+})
+
+app.get('/users', async (req, res) => {
+    res.send(users)
+})
+
+
+app.get('/entrys', async (req, res) => {
+    const {mail} = req.body
+    const user = users.find(e => e.mail === mail)
+
+    res.send(user.movs)
+})
+
+app.post('/entrys', async (req, res) => {
+   const {day, title, multiply, amount, mail} = req.body
+   const user = users.find((e) => e.mail === mail)
+
+   user.movs.push({day: day, title: title, multiply: multiply, amount: amount})
+   res.send(user.movs)
+})
+
+const port = 5000;
+app.listen(port, () => console.log(`Você está utilizando a porta ${port}`))
