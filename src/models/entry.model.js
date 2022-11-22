5e4c9f7b1166d532f0dcdfecaef4199b045f5ee4
Igor Guimarães <igorgui.tec@gmail.com>
2022-11-22 16:53:56 -0300

    Schemas

--- src/models/entry.model.js
+++ src/models/entry.model.js
@@ -1,2 +1,9 @@
-import Joi from "joi";
+import joi from "joi";
 
+const entrySchema = joi.object({
+    day: joi.string.required(),
+    title: joi.string.required().min(5),
+    multiply: joi.number.required(),
+    amount: joi.number.required(),
+    mail: joi.string.mail().required()
+})
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,1 +1,7 @@
-import Joi from "joi";
+import joi from "joi";
+
+const userSchema = joi.object({
+    name: joi.string.required().min(3),
+    password: joi.string.required().min(5),
+    mail: joi.string.required(),
+})
