555e8ca4a4db797f4fa727ac11e977ac67eafe40
Igor Guimarães <igorgui.tec@gmail.com>
2022-11-22 17:47:52 -0300

    correção, router

--- src/routers/entrys.router.js
+++ src/routers/entrys.router.js
@@ -1,9 +1,11 @@
 import { Router } from 'express'
+import { getEntrys, postEntry } from '../controllers/entrys.controllers.js'
 
 const router = Router()
 
 
-router.post('/entrys',)
+router.post('/entrys',postEntry)
+router.get('/entrys', getEntrys)
 
 
 export default router
